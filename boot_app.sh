1827ee4cb0ddc2b122f2a6ee0d75766b62460b6f
Benjamin Favre <ben@webdesign29.net>
2016-10-13 12:36:00 +0200

    push version

---
+++ boot_app.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+export APP_DATABASE_HOST="db"
+
+echo "App booting"
+
+echo "Composer installing ..."
+cd /var/www/html/app/ && \
+composer install
+
+echo "Setting up Nginx ..."
+rm /etc/nginx/sites-enabled/*
+mv /var/www/html/nginx.conf /etc/nginx/sites-available/default.conf
+ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
+
+echo "Secure permissions ..."
+chown -R www-app:www-data /var/www/html/
+
+echo "Make Upload path writeable ..."
+chown 33:33 -R /var/www/html/web/
+chown 33:33 -R /var/www/html/app/cache/
+
+echo "Booting ..."
+date > /var/www/html/boottime.txt
+
+echo "All startup tasks have been run !"
---
+++ maintenance.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "App maintenance script"
+sv up nginx
